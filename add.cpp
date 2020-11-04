75fda93f5a6af6bf1d390c4606968662ae9e50bb
jaysolanki8 <73906363+jaysolanki8@users.noreply.github.com>
2020-11-04 22:39:54 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sumOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    sumOfTwoNumbers = a + b;
+
+    cout << a << " + " <<  b << " = " << sumOfTwoNumbers;     
+
+    return 0;
+}
