7b732c8bdc0be08647700f2cc88c1374c61cfc86
jaysolanki8 <73906363+jaysolanki8@users.noreply.github.com>
2020-12-04 00:21:34 +0530

    Add files via upload

---
+++ methods.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std;  
+class Student {  
+   public:  
+       int roll_no;      
+       string name;      
+       void insert(int i, string n)    
+        {    
+            roll_no= i;    
+            name = n;    
+        }    
+       void display()    
+        {    
+            cout<<roll_no<<"  "<<name<<endl;    
+        }    
+};  
+int main()
+{  
+    Student s1;   
+    Student s2;
+    s1.insert(49, "Jay");    
+    s2.insert(58, "Harsh");    
+    s1.display();    
+    s2.display();  
+    return 0;  
+}  
